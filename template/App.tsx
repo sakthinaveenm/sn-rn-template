0cbbffd03a884529524d9a06e9d9e0e896c7680d
sakthinaveenm <sakthinaveenm@gmail.com>
2023-02-13 11:28:52 +0530

    This is a new commit for what I originally planned to be amended

---
+++ template/App.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello this is Sakthi Naveen Template</Text>
+    </View>
+  )
+}
+
+export default App
